88706a12a60adeb406b59000ecc260abcc9e3c85
Lior Shavit <l.shavitq@gmail.com>
2024-03-23 09:23:55 +0200

    1
    11111

--- app/src/main/java/com/example/quietheart/FourthActivity.java
+++ app/src/main/java/com/example/quietheart/FourthActivity.java
@@ -1,9 +1,9 @@
 package com.example.quietheart;
 
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.Window;
-import android.widget.Toast;
-
+import android.widget.MediaController;
+import android.widget.VideoView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FourthActivity extends AppCompatActivity {
@@ -11,16 +11,26 @@ public class FourthActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_third);
+        setContentView(R.layout.activity_fourth);
 
-        Window window = getWindow();
-        window.setStatusBarColor(getResources().getColor(R.color.blue));
+        // Получаем путь к видео из ресурсов res/raw
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.hand;
 
+        // Находим видео-вью в макете
+        VideoView videoView = findViewById(R.id.videoView);
 
-    }
+        // Устанавливаем путь к видеофайлу
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
 
-    private void showToast(String message) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        // Создаем медиа-контроллер
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+
+        // Устанавливаем медиа-контроллер для видео-вью
+        videoView.setMediaController(mediaController);
+
+        // Начинаем воспроизведение видео
+        videoView.start();
     }
 }
-
